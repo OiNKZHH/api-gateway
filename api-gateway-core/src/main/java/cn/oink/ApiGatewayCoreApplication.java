e90fb633ebbd8788c3dbe7ed69a5d82c4671935d
OiNK <961969414@qq.com>
2024-07-24 23:35:12 +0800

    feat（会话处理程序）：添加基本处理程序和会话服务器实现 -实现BaseHandler以进行会话处理。 -为管道设置添加SessionChannelInitializer。 -创建会话服务器以启动会话服务器。 -引入SessionServerHandler来处理HTTP请求。 -重构代码以提高可读性和维护性。 重大变化：需要使用新的会话处理基础设施。

---
+++ api-gateway-core/src/main/java/cn/oink/ApiGatewayCoreApplication.java
@@ -0,0 +1,13 @@
+package cn.oink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayCoreApplication.class, args);
+	}
+
+}
